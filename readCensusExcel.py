22aa410d5cff7739c11aab61ff730c9ab760b65e
Azimovv <suggzugg7375@gmail.com>
2019-11-20 11:54:45 -0500

    fixed small spacing formatting error

--- readCensusExcel.py
+++ readCensusExcel.py
@@ -1,6 +1,7 @@
 # Tabulates population and number of census tracts for each county
 
 import openpyxl, pprint
+
 print("Opening workbook...")
 wb = openpyxl.load_workbook('censuspopdata.xlsx')
 sheet = wb['Population by Census Tract']
